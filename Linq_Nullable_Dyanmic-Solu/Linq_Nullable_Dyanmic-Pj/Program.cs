c29665f5c9525f5e79b3f04c17ffbc5127952040
Vinci-da-Gama <11582145@student.uts.edu.au>
2018-02-14 14:11:50 +1100

    ta

---
+++ Linq_Nullable_Dyanmic-Solu/Linq_Nullable_Dyanmic-Pj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linq_Nullable_Dyanmic_Pj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
